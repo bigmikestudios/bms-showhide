2866d3c46320f64bd3b6da9d6038ef7f0d81d24a
Mike Lathrop <bigmike@bigmikestudios.com>
2011-11-25 09:50:47 -0800

    First Commit

---
+++ bms_showhide.js
@@ -0,0 +1,23 @@
+// JavaScript Document
+
+jQuery(document).ready(function() {
+	// set up readmore toggle
+	jQuery("div.bms-sh div.bms-sh-label").each(function(){
+		var new_content = '<a href="javascript: void(0);">'+jQuery(this).html() + '</a>';
+		jQuery(this).html(new_content);
+	});
+	
+	jQuery("div.bms-sh div.bms-sh-moreinfo").hide();
+	jQuery('div.bms-sh div.bms-sh-label a').click(function(){
+		jQuery(this).parent().parent().children('div.bms-sh-moreinfo').slideToggle('fast', 'linear');
+		var is_open = jQuery(this).hasClass('bms-sh-is_open');
+		if (is_open)
+		{
+			jQuery(this).removeClass('bms-sh-is_open');
+		}
+		else
+		{
+			jQuery(this).addClass('bms-sh-is_open');
+		}
+	});
+});
---
+++ bms_showhide.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @package BMS_Showhide
+ * @author Mike Lathrop
+ * @version 1.5.1
+ */
+/*
+Plugin Name: BMS Showhide
+Plugin URI: http://bigmikestudios.com
+Description: Creates a shortcode to display jquery "show/hide" divs
+Version: 0.0.1
+Author URI: http://bigmikestudios.com
+*/
+
+$cr = "\r\n";
+
+// include stuff
+
+
+wp_register_script('bms_showhide', plugins_url() .'/bms_showhide/bms_showhide.js', array('jquery'));
+wp_enqueue_script('bms_showhide');
+
+wp_register_style('bms_showhide', plugins_url() .'/bms_showhide/bms_showhide.css');
+wp_enqueue_style('bms_showhide');
+
+// shortcode for readmore
+function bms_showhide ($atts, $content=null)
+{
+	extract( shortcode_atts( array(
+      'label' => 'Read more&gt;&gt;',
+      ), $atts ) );
+	$return = "<div class='bms-sh'>".$cr;
+	$return .= "<div class='bms-sh-label'>".$cr;
+	$content = ltrim($content, "</p>");
+	$content = rtrim($content, "<p>");
+	if (isset($atts['readless'])) {
+		$return .= "<span class='bms-sh-more'>".$label."</span><span class='bms-sh-less'>".$atts['readless']."</span>".$cr;
+	} else {
+		$return .= $atts['label'];
+	}
+	$return .= "</div><!-- /.bms-sh-label -->";
+	$return .= "<div class='bms-sh-moreinfo'>";
+	$return .= $content;
+	$return .= "</div><!-- /.bms-sh-moreinfo -->".$cr;
+	$return .= "</div><!-- /.bms-sh -->".$cr;
+	return $return;
+	
+}
+add_shortcode('bms_showhide', 'bms_showhide');
+
+?>
