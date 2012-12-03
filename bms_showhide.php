9926f4fd97b88b669c1ce505f568a6ddaffb0aed
Mike Lathrop <bigmike@bigmikestudios.com>
2012-12-03 14:58:52 -0800

    Added "label-wrapper" parameter

--- bms_showhide.php
+++ bms_showhide.php
@@ -17,10 +17,10 @@ $cr = "\r\n";
 // include stuff
 
 
-wp_register_script('bms_showhide', plugins_url() .'/bms_showhide/bms_showhide.js', array('jquery'));
+wp_register_script('bms_showhide', plugins_url() .'/bms-showhide/bms_showhide.js', array('jquery'));
 wp_enqueue_script('bms_showhide');
 
-wp_register_style('bms_showhide', plugins_url() .'/bms_showhide/bms_showhide.css');
+wp_register_style('bms_showhide', plugins_url() .'/bms-showhide/bms_showhide.css');
 wp_enqueue_style('bms_showhide');
 
 // shortcode for readmore
@@ -28,16 +28,22 @@ function bms_showhide ($atts, $content=null)
 {
 	extract( shortcode_atts( array(
       'label' => 'Read more&gt;&gt;',
+	  'readless' => false,
+	  'label_wrapper' => false,
       ), $atts ) );
 	$return = "<div class='bms-sh'>".$cr;
 	$return .= "<div class='bms-sh-label'>".$cr;
 	$content = ltrim($content, "</p>");
 	$content = rtrim($content, "<p>");
-	if (isset($atts['readless'])) {
-		$return .= "<span class='bms-sh-more'>".$label."</span><span class='bms-sh-less'>".$atts['readless']."</span>".$cr;
+	if ($label_wrapper) $return.= "<".$label_wrapper.">";
+	if ($readless) {
+		$return .= "<span class='bms-sh-more'>".$label."</span><span class='bms-sh-less'>".$readless."</span>".$cr;
 	} else {
 		$return .= $atts['label'];
 	}
+	if ($label_wrapper) $return.= "</".$label_wrapper.">";
+	// execute shortcodes in content
+	$content = do_shortcode($content);
 	$return .= "</div><!-- /.bms-sh-label -->";
 	$return .= "<div class='bms-sh-moreinfo'>";
 	$return .= $content;
